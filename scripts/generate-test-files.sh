0f559394e64550cf857b9f8ad45164c1ca8588ff
LooSheng <loosheng@outlook.com>
2023-09-15 21:32:45 +0800

    fix: There is a logical error in getFilePaths.

--- scripts/generate-test-files.sh
+++ scripts/generate-test-files.sh
@@ -6,3 +6,24 @@ touch test/foo/a.js
 touch test/foo/b.txt
 touch test/a.txt
 touch test/b.js
+
+
+# issues #6
+mkdir -p test/output-9071a1942d0d334aa224a1370d98e18015782d6f/rechtsformen/test
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/404.html
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/begriffe.html
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/datenschutz.html
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/feed_rss_created.xml
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/feed_rss_updated.xml
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/impressum.html
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/index.html
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/rechtsformen/index.html
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/rechtsformen/test/1.html
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/rechtsformen/test/2.html
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/rechtsformen/test/3.html
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/rechtsformen/test/index.html
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/sitemap.xml
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/sitemap.xml.gz
+mkdir -p test/output-9071a1942d0d334aa224a1370d98e18015782d6f/search
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/search/search_index.js
+touch test/output-9071a1942d0d334aa224a1370d98e18015782d6f/search/search_index.json
